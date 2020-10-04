10be94b7e267d8497d6a7161cf13074ebd781b71
Muhammad Mohsin <mmohsinkhalid@outlook.com>
2020-10-04 23:07:20 +0500

    add hello world netlify func

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const [val, setVal] = useState({});
+
+  useEffect(()=>{
+    fetch(`/.netlify/functions/hello?name=from new react app`)
+    .then(res=>res.json())
+    .then(obj=>setVal(obj))
+  },[])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h3>{val.message}</h3>
     </div>
   );
 }
